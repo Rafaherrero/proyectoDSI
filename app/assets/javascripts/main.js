b62256909280e427070337c6fe6890fddfd2ec22
Rafa Herrero <rafael.herrero.13@ull.edu.es>
2016-05-10 11:29:19 +0000

    Funcionando caratulas

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,10 +1,1 @@
 // Hola Mundo
-
-var id3 = require('id3js');
- 
-id3({ file: '23.mp3', type: id3.OPEN_LOCAL }, function(err, tags) {
-	// tags now contains your ID3 tags 
-	console.log(tags);
-});
-
-console.log(id3);
