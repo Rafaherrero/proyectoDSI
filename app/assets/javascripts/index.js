a3a5d9c812591e252421fea5c2b5b43068f18018
Rafa Herrero <rafael.herrero.13@ull.edu.es>
2016-05-15 10:29:08 +0000

    Funcionando mas de una cancion

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,7 +1,10 @@
 jQuery(document).ready(function($) {
       let datos_canciones;
-      let myaudio = new Audio();
-      $(() => {
+      let myaudio = document.getElementById('miaudio');
+      let cnt=0;
+      var rangeSlider = document.getElementById('rango_tiempo');
+      let id_cancion_actual = 0;
+
       $.ajax(
       {     
             type: 'GET',
@@ -16,22 +19,44 @@ jQuery(document).ready(function($) {
                         $('.carousel-item').first().addClass('active');
                   }
                   $('#carousel_col').carousel();
+                  id_cancion_actual = datos_canciones[0]._id;
                   carga_cancion(datos_canciones[0]._id);
             },
-            fail: function(){
+            error: function(){
                   alert("No se ha loguineado en la web");
             }
       });
       
+      function carga_cancion (ident){
+      $.ajax(
+      {     
+            type: 'GET',
+            url: `/songs/${ident}`,
+            success: function (cancionmp3){
+                  //http://stackoverflow.com/questions/17762763/play-wav-sound-file-encoded-in-base64-with-javascript
+                  myaudio.setAttribute('src', "data:audio/mp3;base64," + cancionmp3);
+                  myaudio.load();
+                  $('.outer').fadeOut(2000);
+            },
       });
+      }
 	
-	/*
-	http://stackoverflow.com/questions/17762763/play-wav-sound-file-encoded-in-base64-with-javascript
-	*/
-      
-      function makeCarousel(photo) {
+      //http://stackoverflow.com/questions/7346827/javascript-find-array-index-with-value
+      function IndexByKey(arraytosearch, key, valuetosearch) {
 
+            for (var i = 0; i < arraytosearch.length; i++) {
+
+            if (arraytosearch[i][key] == valuetosearch) {
+                  return i;
+            }
+            }
+            return null;
+      }
+      
+      //http://stackoverflow.com/questions/34404127/trouble-initializing-a-dynamically-built-materialize-carousel
+      function makeCarousel(photo) {
              var newItem = $("<a>").addClass("carousel-item");
+             newItem.attr("id", photo._id);
              var Img = $("<img>").attr("src", "data:image/png;base64," + photo.image.data);
              
              newItem.attr("href", "#");
@@ -41,34 +66,26 @@ jQuery(document).ready(function($) {
              return newItem;
       }
       
-      function carga_cancion (ident){
-      $.ajax(
-      {     
-            type: 'GET',
-            url: `/songs/${ident}`,
-            success: function (cancionmp3){
-                  myaudio = new Audio("data:audio/mp3;base64," + cancionmp3);
-            },
-            
-            statusCode: {
-                  200: function() {
-                  actualizar_datos();
-                  }
-            },
-            complete: function(){
-                  actualizar_datos();
-            }
-      });
+      function formatTime_back(time){
+            let a = time.split(':');
+            return ((parseInt(a[0])*60)+parseInt(a[1]))
       }
-
-	var rangeSlider = document.getElementById('rango_tiempo');
-	
-	function actualizar_datos (){
-
-            $('#tiempo_cancion_restante').text(formatTime(myaudio.duration));
-            $('#tiempo_cancion_reproducido').text(formatTime(0));
-            
-            $('#cargando').remove();
+      
+      function cambiar_por_play (){
+            myaudio.play();
+            $('#boton_p').replaceWith('<a class="btn-floating btn-large waves-effect" id="boton_p"><i id="boton_p_material_icons" class="material-icons">pause</i></a>');
+      }
+      
+      function cambiar_por_pause (){
+            myaudio.pause();
+	      $('#boton_p').replaceWith('<a class="btn-floating btn-large waves-effect" id="boton_p"><i id="boton_p_material_icons" class="material-icons">play_arrow</i></a>');
+      }
+      
+      myaudio.addEventListener("loadedmetadata", function() {
+            if(cnt>0){
+            rangeSlider.noUiSlider.destroy();
+            cambiar_por_play();
+            }
             
 	      noUiSlider.create(rangeSlider, {
                   start: [0],
@@ -91,24 +108,14 @@ jQuery(document).ready(function($) {
 	                  }
                   }
             });
-            
-            rangeSlider.noUiSlider.on('slide', function(){
-                  myaudio.currentTime = formatTime_back(rangeSlider.noUiSlider.get());
-            });
-      };
-
-      myaudio.addEventListener('timeupdate', function () {
-            let minutes, seconds;
-            $('#tiempo_cancion_reproducido').text(formatTime(myaudio.currentTime));
-            $('#tiempo_cancion_restante').text(formatTime(myaudio.duration - myaudio.currentTime));
-            
-            rangeSlider.noUiSlider.set([myaudio.currentTime]);
+            actualizar_datos();
+            cnt++;
       });
       
       myaudio.onended = function() {
-            myaudio.pause();
-	      $('#boton_p').replaceWith('<a class="btn-floating btn-large waves-effect" id="boton_p"><i id="boton_p_material_icons" class="material-icons">play_arrow</i></a>');
+            cambiar_por_pause();
 	      myaudio.currentTime = 0;
+	      siguiente_cancion();
       };
       
       function formatTime(seconds) {
@@ -119,22 +126,48 @@ jQuery(document).ready(function($) {
             return minutes + ":" + seconds;
       }
       
-      function formatTime_back(time){
-            let a = time.split(':');
-            return ((parseInt(a[0])*60)+parseInt(a[1]))
-      }
+      myaudio.addEventListener('timeupdate', function () {
+            let minutes, seconds;
+            $('#tiempo_cancion_reproducido').text(formatTime(myaudio.currentTime));
+            $('#tiempo_cancion_restante').text(formatTime(myaudio.duration - myaudio.currentTime));
+            
+            rangeSlider.noUiSlider.set([myaudio.currentTime]);
+            
+            });
+
+  	function actualizar_datos() {
+
+            $('#tiempo_cancion_restante').text(formatTime(myaudio.duration));
+            $('#tiempo_cancion_reproducido').text(formatTime(0));
+            
+            rangeSlider.noUiSlider.on('slide', function(){
+                  myaudio.currentTime = formatTime_back(rangeSlider.noUiSlider.get());
+            });
+            
+            
+      };
       
 	$(document).on("click", "#boton_p", function() {
-
-	    if ($('#boton_p').text()==('play_arrow')){
-		myaudio.play();
-            $('#boton_p').replaceWith('<a class="btn-floating btn-large waves-effect" id="boton_p"><i id="boton_p_material_icons" class="material-icons">pause</i></a>');
+	      
+	   if ($('#boton_p').text()==('play_arrow')){
+		cambiar_por_play();
 	    }
 	    else{
-	      myaudio.pause();
-	      $('#boton_p').replaceWith('<a class="btn-floating btn-large waves-effect" id="boton_p"><i id="boton_p_material_icons" class="material-icons">play_arrow</i></a>');
+	      cambiar_por_pause();
 	    }
 	});
+	function siguiente_cancion (){
+	      cambiar_por_pause();
+	      id_cancion_actual = datos_canciones[(IndexByKey(datos_canciones,"_id",id_cancion_actual)+1)%datos_canciones.length]._id;
+	      $('.carousel').carousel('next');
+	      carga_cancion(id_cancion_actual);
+	      myaudio.currentTime = 0;
+	};
+	
+	$(document).on("click", "#boton_siguiente", function() {
+	      siguiente_cancion();
+	});
+	
 });
 	
 	function inicializarcarousel (){
